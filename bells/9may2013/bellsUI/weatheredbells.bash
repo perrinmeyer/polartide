d176bb3d26b366288b8b1addffb13c1217056762
Perrin Meyer <perrin@mac-perrin2.eng.msli.com>
2013-05-09 15:04:10 -0700

    shell script to weather the bells from cc this morning

---
+++ bells/9may2013/bellsUI/weatheredbells.bash
@@ -0,0 +1,67 @@
+
+# bells1.wav 
+./psmAirAtten  8.3 52 101.325 33 48  |sox bells1.wav bells1_ker_33m.wav fir -
+./psmAirAtten 31.1 98 101.325 33 48  |sox bells1.wav bells1_gan_33m.wav fir -
+./psmAirAtten 23.8 53 101.325 33 48  |sox bells1.wav bells1_ven_33m.wav fir -
+./psmAirAtten -5.0 40 101.325 33 48  |sox bells1.wav bells1_rey_33m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 50 48  |sox bells1.wav bells1_ker_50m.wav fir -
+./psmAirAtten 31.1 98 101.325 50 48  |sox bells1.wav bells1_gan_50m.wav fir -
+./psmAirAtten 23.8 53 101.325 50 48  |sox bells1.wav bells1_ven_50m.wav fir -
+./psmAirAtten -5.0 40 101.325 50 48  |sox bells1.wav bells1_rey_50m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 100 48  |sox bells1.wav bells1_ker_100m.wav fir -
+./psmAirAtten 31.1 98 101.325 100 48  |sox bells1.wav bells1_gan_100m.wav fir -
+./psmAirAtten 23.8 53 101.325 100 48  |sox bells1.wav bells1_ven_100m.wav fir -
+./psmAirAtten -5.0 40 101.325 100 48  |sox bells1.wav bells1_rey_100m.wav fir -
+
+# bells2.wav 
+./psmAirAtten  8.3 52 101.325 33 48  |sox bells2.wav bells2_ker_33m.wav fir -
+./psmAirAtten 31.1 98 101.325 33 48  |sox bells2.wav bells2_gan_33m.wav fir -
+./psmAirAtten 23.8 53 101.325 33 48  |sox bells2.wav bells2_ven_33m.wav fir -
+./psmAirAtten -5.0 40 101.325 33 48  |sox bells2.wav bells2_rey_33m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 50 48  |sox bells2.wav bells2_ker_50m.wav fir -
+./psmAirAtten 31.1 98 101.325 50 48  |sox bells2.wav bells2_gan_50m.wav fir -
+./psmAirAtten 23.8 53 101.325 50 48  |sox bells2.wav bells2_ven_50m.wav fir -
+./psmAirAtten -5.0 40 101.325 50 48  |sox bells2.wav bells2_rey_50m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 100 48  |sox bells2.wav bells2_ker_100m.wav fir -
+./psmAirAtten 31.1 98 101.325 100 48  |sox bells2.wav bells2_gan_100m.wav fir -
+./psmAirAtten 23.8 53 101.325 100 48  |sox bells2.wav bells2_ven_100m.wav fir -
+./psmAirAtten -5.0 40 101.325 100 48  |sox bells2.wav bells2_rey_100m.wav fir -
+
+# bells3.wav 
+./psmAirAtten  8.3 52 101.325 33 48  |sox bells3.wav bells3_ker_33m.wav fir -
+./psmAirAtten 31.1 98 101.325 33 48  |sox bells3.wav bells3_gan_33m.wav fir -
+./psmAirAtten 23.8 53 101.325 33 48  |sox bells3.wav bells3_ven_33m.wav fir -
+./psmAirAtten -5.0 40 101.325 33 48  |sox bells3.wav bells3_rey_33m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 50 48  |sox bells3.wav bells3_ker_50m.wav fir -
+./psmAirAtten 31.1 98 101.325 50 48  |sox bells3.wav bells3_gan_50m.wav fir -
+./psmAirAtten 23.8 53 101.325 50 48  |sox bells3.wav bells3_ven_50m.wav fir -
+./psmAirAtten -5.0 40 101.325 50 48  |sox bells3.wav bells3_rey_50m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 100 48  |sox bells3.wav bells3_ker_100m.wav fir -
+./psmAirAtten 31.1 98 101.325 100 48  |sox bells3.wav bells3_gan_100m.wav fir -
+./psmAirAtten 23.8 53 101.325 100 48  |sox bells3.wav bells3_ven_100m.wav fir -
+./psmAirAtten -5.0 40 101.325 100 48  |sox bells3.wav bells3_rey_100m.wav fir -
+
+# bells4.wav 
+./psmAirAtten  8.3 52 101.325 33 48  |sox bells4.wav bells4_ker_33m.wav fir -
+./psmAirAtten 31.1 98 101.325 33 48  |sox bells4.wav bells4_gan_33m.wav fir -
+./psmAirAtten 23.8 53 101.325 33 48  |sox bells4.wav bells4_ven_33m.wav fir -
+./psmAirAtten -5.0 40 101.325 33 48  |sox bells4.wav bells4_rey_33m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 50 48  |sox bells4.wav bells4_ker_50m.wav fir -
+./psmAirAtten 31.1 98 101.325 50 48  |sox bells4.wav bells4_gan_50m.wav fir -
+./psmAirAtten 23.8 53 101.325 50 48  |sox bells4.wav bells4_ven_50m.wav fir -
+./psmAirAtten -5.0 40 101.325 50 48  |sox bells4.wav bells4_rey_50m.wav fir -
+
+./psmAirAtten  8.3 52 101.325 100 48  |sox bells4.wav bells4_ker_100m.wav fir -
+./psmAirAtten 31.1 98 101.325 100 48  |sox bells4.wav bells4_gan_100m.wav fir -
+./psmAirAtten 23.8 53 101.325 100 48  |sox bells4.wav bells4_ven_100m.wav fir -
+./psmAirAtten -5.0 40 101.325 100 48  |sox bells4.wav bells4_rey_100m.wav fir -
+
+
+
